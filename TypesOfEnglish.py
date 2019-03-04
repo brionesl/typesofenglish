f516193a81e7e1b760c9561e801b2f67d74f8206
Luis Briones <37453554+brionesl@users.noreply.github.com>
2019-03-04 18:17:50 -0500

    Update TypesOfEnglish.py

--- TypesOfEnglish.py
+++ TypesOfEnglish.py
@@ -2,86 +2,107 @@
 # Author: Luis Briones
 
 import requests
-import sys
-import os
 import re
 
+class Request:
+    '''
+        Class returns response from request. Split into two methods because one uses get and the other post
+        depending on the type of request
+    '''
+    def __init__ (self):
+        # Class variable
+        self.query_items = {}
+        pass
 
-# Connect to  API end point and get product data
-def get_product(query, productids):
-    request = requests.post('https://www.teacherspayteachers.com/graph/graphql', json={'query': query, 'variables': productids})
-    if request.status_code == 200:
-        return request.json()
-    else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
-
-
-# Download list of words from gist
-def get_words():
-    try:
-        americanrequest = requests.get(
-            'https://gist.githubusercontent.com/mdg/aa4c9070ff3dbeaa5d4613cba05c2faf/raw/c9f1795048a9d7f841079aca2a66f14ef3e7002b/american-words.txt')
-        britishrequest = requests.get(
-            'https://gist.githubusercontent.com/mdg/aa4c9070ff3dbeaa5d4613cba05c2faf/raw/c9f1795048a9d7f841079aca2a66f14ef3e7002b/british-words.txt')
-
-        if americanrequest.status_code == 200 and britishrequest.status_code == 200:
-            # Remove blank lines and split list by new lines
-            americanwords = list(filter(None, (americanrequest.text).split('\n')))
-            britishwords = list(filter(None, (britishrequest.text).split('\n')))
-        else:
-            raise Exception("Failed to get word list \n americanrequest: {} \n britishrequest: {}".
-                            format(americanrequest.status_code, britishrequest.status_code))
-
-        return {'americanwords': americanwords, 'britishwords': britishwords}
-    except requests.exceptions.RequestException as e:
-        print(e)
-        sys.exit(1)
+    def get_items_to_query(self, url, requestType):
+        try:
+            response = requests.get(url)
+            if response.status_code == 200:
+                item_list = list(filter(None, (response.text).split('\n')))
+                self.query_items[requestType] = item_list
+            else:
+                raise Exception('Failed to get word list \n {}: {}'.format(requestType, response.status_code))
+        except requests.exceptions.RequestException as e:
+            print(e)
+    
+    
+    # Connect to  API end point and get product data
+    def get_product_info(self, url, query, productids):
+        try:
+            response = requests.post(url, json={'query': query, 'variables': productids})
+            if response.status_code == 200:
+                return response.json()
+            else:
+                raise Exception("Query failed to run by returning code of {}. {}".format(response.status_code, query))
+        except requests.exceptions.RequestException as e:
+            print(e)
 
 
 # Determine type of English
-def check_type_of_english(productstring, wordlist):
+def check_type_of_english(productstring, word_dict):
     americancount = 0
     britishcount = 0
 
-    try:
-        productstring = str(productstring)
+    productstring = str(productstring)
 
-        # Regular expressions below check word boundaries and plurals
-
-        # American check
-        for aword in wordlist.get('americanwords', ''):  # regular expresison checks word boundaries and plural
-            amer_regex = re.findall(r'\b' + re.escape(aword) + r's?\b', productstring, re.IGNORECASE)
-            if amer_regex:
+    # Loop through  hash map keys to check if word's in the product string
+    for word in word_dict.keys():
+        # Regular expression checks word boundaries and plurals
+        word_found = re.findall(r'\b' + re.escape(word) + r's?\b', productstring, re.IGNORECASE)
+        
+        # Keep count of american and british words found
+        if word_found:
+            if word_dict[word] == 'american':
                 americancount += 1
-
-        # British check
-        for bword in wordlist.get('britishwords', ''):
-            brit_regex = re.findall(r'\b' + re.escape(bword) + r's?\b', productstring, re.IGNORECASE)
-            if brit_regex:
+            else:
                 britishcount += 1
 
-        if americancount > 0 and britishcount == 0:
-            return 'American English'
-        elif britishcount > 0 and americancount == 0:
-            return 'British English'
-        elif americancount > 0 and britishcount > 0:
-            return 'Mixed British and American English'
-        else:
-            return 'Unknown'
-
-    except ValueError as e:
-        print('Variable provided was not a string. Error: {0}'.format(e))
+    if americancount > 0 and britishcount == 0:
+        return 'American English'
+    elif britishcount > 0 and americancount == 0:
+        return 'British English'
+    elif americancount > 0 and britishcount > 0:
+        return 'Mixed British and American English'
+    else:
+        return 'Unknown'
 
 
 def main():
-    currentpath = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-    # Open product id list from file
-    with open(os.path.join(currentpath, 'products.txt'), 'r') as f:
-        productidimport = [line.rstrip('\n') for line in f]
+    # Dictionary with all the URLS used throughout program
+    url = {'americanwords': 'https://raw.githubusercontent.com/brionesl/typesofenglish/master/american-words.txt',
+           'britishwords': 'https://raw.githubusercontent.com/brionesl/typesofenglish/master/british-words.txt',
+           'products': 'https://raw.githubusercontent.com/brionesl/typesofenglish/master/products.txt',
+           'product_api_url':  'https://www.teacherspayteachers.com/graph/graphql'}
+
+    # Instantiate Request Class
+    request = Request()
+
+    # Response from calls to urls, are added to class variable "query_items"
+    for requestType, link in url.items():
+        if 'product_api_url' not in requestType:
+            request.get_items_to_query(link, requestType=requestType)
+    
+    # Create hash map for words and the types of english
+    ''' Example:
+        {
+            'color': 'american',
+            'colour': 'british'
+        }
+    '''
+    american_dict = {}
+    british_dict = {}
+    for word in request.query_items['americanwords']:
+        american_dict[word] = 'american'
+
+    for word in request.query_items['britishwords']:
+        british_dict[word] = 'british'
+
+    # Combine hash maps
+    word_dict = {**american_dict, **british_dict}
 
     # GraphQL Variables
-    productids = {"productIds": productidimport}
+    productids = {"productIds": request.query_items['products']}
 
     # GraphQL query
     query = """
@@ -93,27 +114,21 @@ def main():
         }
     }
     """
-
     # Get product information
-    product = get_product(query, productids)
-    productinfo = product.get('data', {}).get('products', {})
-
     productoutput= []
-
-    # Get lists of american and british words
-    wordlist = get_words()
+    product_list = request.get_product_info(url['product_api_url'], query, productids)
+    productinfo = product_list['data']['products']
 
     for product in productinfo:
         # productoutput dictionary will hold ids mapped to type of english
         productoutput.append({
-                            "productid": product.get('id', ''),
-                            "englishtype": check_type_of_english(product.get('name', '') + ' ' +
-                                                                 product.get('description', ''), wordlist)
+                            "productid": product['id'],
+                            "englishtype": check_type_of_english(product['name'] + ' ' +
+                                                                 product['description'], word_dict)
                             })
 
     # Output dictionary content
-    [print('{0} - {1}'.format(product.get('productid', ''), product.get('englishtype', ''))) for product in
-     productoutput]
+    [print('{0} - {1}'.format(product['productid'], product['englishtype'])) for product in productoutput]
 
 
 if __name__ == "__main__":
